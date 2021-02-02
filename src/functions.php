39960b7e68216e61fdec1a03040284285786b824
luke <lukeme@users.noreply.github.com>
2021-02-02 11:06:37 +0800

    身份证校验

--- src/functions.php
+++ src/functions.php
@@ -20,3 +20,36 @@ if(!function_exists('dump')){
 	}
 }
 
+//是否是身份证
+if(!function_exists('is_idcard')){
+	function is_idcard($id)
+	{
+	    $id = strtoupper($id);
+	    $regx = "/(^\d{15}$)|(^\d{17}([0-9X])$)/";
+	    if (!preg_match($regx, $id)) {
+		return false;
+	    } else if (15 === strlen($id)) {
+		$regx = "/^(\d{6})+(\d{2})+(\d{2})+(\d{2})+(\d{3})$/";
+		preg_match($regx, $id, $birthday);
+		return !strtotime($birthday[1]); //检查生日日期
+	    } else {
+		$regx = "/^\d{6}(\d{8})\d{3}[0-9X]$/";
+		preg_match($regx, $id, $birthday);
+		if (!strtotime($birthday[1])) //检查生日日期是否正确
+		{
+		    return false;
+		} else {
+		    //校验位按照ISO 7064:1983.MOD 11-2的规定生成，X可以认为是数字10。
+		    $powerArr = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
+		    $checkArr = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
+		    $sign = 0;
+		    for ($i = 0; $i < 17; $i++) {
+			$b = (int)$id{$i};
+			$w = $powerArr[$i];
+			$sign += $b * $w;
+		    }
+		    return $checkArr[$sign % 11] === $id{17};
+		}
+	    }
+	}
+}
