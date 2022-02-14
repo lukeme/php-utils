423d6eeb8f1d008ed335ae112012a939c8fe5aef
luke <lukeme@users.noreply.github.com>
2022-02-14 11:31:34 +0800

    Update Db.php

--- src/Db.php
+++ src/Db.php
@@ -8,7 +8,7 @@ class Db
 
     function __construct(array $conf)
     {
-		if('sqlsrv'==$conf['driver']){
+	if('sqlsrv'==$conf['driver']){
             $dsn = sprintf('sqlsrv:Server=%s;Database=%s;', $conf['host'], $conf['name']);
         }else{
             $dsn = sprintf('%s:host=%s;port=%s;dbname=%s;', $conf['driver'], $conf['host'], $conf['port'], $conf['name']);
